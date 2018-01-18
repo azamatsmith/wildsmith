785b5812d1730ed8bbf6b440088d383983b9be01
Matthew Smith <azamatsmith@users.noreply.github.com>
2018-01-17 23:56:18 -0700

    Add google site verification (#3)

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -13,6 +13,7 @@ const TemplateWrapper = ({ children }) => (
       meta={[
         { name: 'description', content: 'Sample' },
         { name: 'keywords', content: 'sample, something' },
+        { name: 'google-site-verification', content: '3rWKg8amLd3Yr6eHKJwlWBvHPgOOPJdz7YjhCyBl6s8' },
       ]}
     />
     <Header />
