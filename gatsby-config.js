c615721f52bba2b9f86dc491af726f38964f8c82
Matthew Smith <azamatsmith@gmail.com>
2018-01-17 20:46:13 -0700

    Add gatsby-plugin-resolve-src

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'wildsmith studio site',
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-resolve-src',
+  ],
 };
---
+++ src/components/TitleText/index.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import './TitleText.css'
+
+
+export default class TitleText extends Component {
+  static propTypes = {
+    el: PropTypes.element,
+    size: PropTypes.number,
+  }
+
+  static defaultProps = {
+    element: 'span',
+    size: 71,
+  }
+
+  render() {
+    const { children el, size } = this.props;
+    return (
+      <${el} style={{ fontSize: size }} className="TitleText">
+        {children}
+      </${el}>
+    );
+  }
+}
