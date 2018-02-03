73666f3120b7b518274b38dd135fbe4fc176665e
Matthew Smith <azamatsmith@gmail.com>
2018-02-03 08:56:23 -0700

    Update <Navbar /> - add link transitions

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import GatsbyLink from 'gatsby-link';
-// import './Navbar.css'
+import './Navbar.scss';
 
 
 export default class Navbar extends Component {
@@ -13,41 +13,44 @@ export default class Navbar extends Component {
 
   // PRIVATE
 
-  _getClass = (name) => {
-    const baseClass = "link dim black b f6 f5-ns dib mr3";
+  _getClass = (name, index) => {
+    const baseClass = `Navbar-link link-${index} w-25 link black b f6 f5-ns dib`;
     return this.props.currentRoute === name
-      ?  `${baseClass} blue`
+      ?  `${baseClass} active`
       : baseClass;
   }
 
 
   render() {
     return (
-      <nav className="Navbar">
-        <GatsbyLink
-          className={this._getClass('/')}
-          to="/"
-        >
-          home
-        </GatsbyLink>
-        <GatsbyLink
-          className={this._getClass('/select-work')}
-          to="/select-work"
-        >
-          select work
-        </GatsbyLink>
-        <GatsbyLink
-          className={this._getClass('/who-we-are')}
-          to="/who-we-are"
-        >
-          who we are
-        </GatsbyLink>
-        <GatsbyLink
-          className={this._getClass('/contact')}
-          to="/contact"
-        >
-          contact
-        </GatsbyLink>
+      <nav className="Navbar w-100 flex justify-end">
+        <div className="w-50">
+          <GatsbyLink
+            className={this._getClass('/', 0)}
+            to="/"
+          >
+            home
+          </GatsbyLink>
+          <GatsbyLink
+            className={this._getClass('/select-work', 1)}
+            to="/select-work"
+          >
+            select work
+          </GatsbyLink>
+          <GatsbyLink
+            className={this._getClass('/who-we-are', 2)}
+            to="/who-we-are"
+          >
+            who we are
+          </GatsbyLink>
+          <GatsbyLink
+            className={this._getClass('/contact', 3)}
+            to="/contact"
+          >
+            contact
+          </GatsbyLink>
+          <hr className="Navbar-hr mb0 bg-orange transition-3 bn" style={{ height: '.25rem' }} />
+        </div>
       </nav>
     );
   }
