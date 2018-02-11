460feb5dbb594f9316285fb38d83a0e0a74a6a2b
Matthew Smith <azamatsmith@users.noreply.github.com>
2018-02-11 08:45:48 -0700

    Update Mobile Styles (#9) (#10)
    * Update mobile styles for nav
    
    * Adjust mobile styles on Home
    
    * Add mobile styles on contact page

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import GatsbyLink from 'gatsby-link';
 import './Navbar.scss';
@@ -13,39 +13,33 @@ export default class Navbar extends Component {
   // PRIVATE
 
   _getClass = (name, index) => {
-    const baseClass = `Navbar-link link-${index} link near-black b f6 f5-ns dib pt4`;
+    const baseClass = `Navbar-link link-${index} link near-black b f6 f5-ns dib pt4 tc tl-ns`;
     return this.props.currentRoute === name ? `${baseClass} active` : baseClass;
   };
 
   render() {
     const width = '33%';
     return (
-      <nav className="Navbar w-100 flex justify-end">
-        <div className="w-40">
-          <GatsbyLink
-            className={this._getClass('/', 0)}
-            to="/"
-            style={{ width }}
-          >
+      <nav className="Navbar w-100 flex justify-around justify-end-ns">
+        <div className="w-100   w-40-ns">
+          <GatsbyLink className={this._getClass('/', 0)} to="/" style={{width}}>
             home
           </GatsbyLink>
           <GatsbyLink
             className={this._getClass('/who-we-are', 1)}
             to="/who-we-are"
-            style={{ width }}
-          >
+            style={{width}}>
             who we are
           </GatsbyLink>
           <GatsbyLink
             className={this._getClass('/contact', 2)}
             to="/contact"
-            style={{ width }}
-          >
+            style={{width}}>
             contact
           </GatsbyLink>
           <hr
             className="Navbar-hr mb0 bg-orange transition-3 bn"
-            style={{ height: '.25rem' }}
+            style={{height: '.25rem'}}
           />
         </div>
       </nav>
--- src/routes/Home/index.js
+++ src/routes/Home/index.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import { Instafeed, TitleText } from 'components';
+import React, {Component} from 'react';
+import {Instafeed, TitleText} from 'components';
 import PropTypes from 'prop-types';
 import './Home.scss';
 
@@ -17,7 +17,7 @@ export default class Home extends Component {
   componentDidMount() {
     setTimeout(() => {
       if (this.mounted) {
-        this.setState({ highlightActive: true });
+        this.setState({highlightActive: true});
       }
     }, 1000);
   }
@@ -27,13 +27,13 @@ export default class Home extends Component {
   }
 
   render() {
-    const { highlightActive } = this.state;
-    const highlightClass = `Home-underline h-18px bg-orange absolute ${
+    const {highlightActive} = this.state;
+    const highlightClass = `Home-underline bg-orange absolute ${
       highlightActive ? 'active' : ''
     }`;
     return (
       <div className="Home flex flex-column justify-center items-center">
-        <div className="Home-text-wrapper relative mt6">
+        <div className="Home-text-wrapper relative mb2 mt2 tc mb0-ns mt6-ns">
           <TitleText fontWeight="bold">we are wildsmith.</TitleText>
           <div className={highlightClass} />
         </div>
