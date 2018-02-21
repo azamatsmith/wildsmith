146cad1dfa331bd400ec341e01c76e87d26a4d17
Matthew Smith <azamatsmith@users.noreply.github.com>
2018-02-21 09:09:01 -0700

    Refactor - Query Fragment (#13)
    * Update <WhoWeAre /> - add query fragment
    
    * run format

--- src/pages/who-we-are.js
+++ src/pages/who-we-are.js
@@ -1,15 +1,10 @@
 import React from 'react';
-import { WhoWeAre } from 'routes';
+import {WhoWeAre} from 'routes';
 
-export default ({ data }) => <WhoWeAre {...data} />;
+export default ({data}) => <WhoWeAre {...data} />;
 
 export const query = graphql`
   query ProfileImageQuery {
-    mattProfile: imageSharp(id: { regex: "/matt-profile.png/" }) {
-      ...ProfileImage
-    }
-    rachelProfile: imageSharp(id: { regex: "/rachel-profile.png/" }) {
-      ...ProfileImage
-    }
+    ...ProfileImage
   }
 `;
