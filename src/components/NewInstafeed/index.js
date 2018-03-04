c90806497769e054fd65a0d2797f5d6e1dbc6d5c
Matthew Smith <azamatsmith@gmail.com>
2018-03-04 08:35:26 -0700

    Create Instagram plugin

---
+++ src/components/NewInstafeed/index.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import ImagesType from './types';
+import Img from 'gatsby-image';
+
+export default class NewInstafeed extends React.Component {
+  static propTypes = {
+    ...ImagesType,
+  };
+
+  static defaultProps = {};
+
+  // PRIVATE
+
+  _renderImages = () =>
+    this.props.data.allFile.edges.map(({node}) => (
+      <a href={node.fields.link} target="_blank" rel="noopener noreferrer">
+        <Img
+          className=""
+          outerWrapperClassName="ma3 pointer"
+          onClick={() => console.log('clicked')}
+          resolutions={node.childImageSharp.resolutions}
+        />
+      </a>
+    ));
+
+  render() {
+    console.log('newInstafeed props', this.props);
+    return (
+      <div className="flex flex-wrap mh5 justify-center">
+        {this._renderImages()}
+      </div>
+    );
+  }
+}
+
+export const instagramImagesFragment = graphql`
+  fragment InstagramImages on RootQueryType {
+    allFile(filter: {fields: {InstagramImage: {eq: "true"}}}) {
+      edges {
+        node {
+          fields {
+            link
+          }
+          childImageSharp {
+            resolutions(height: 450) {
+              ...GatsbyImageSharpResolutions
+            }
+          }
+        }
+      }
+    }
+  }
+`;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,12 @@
 import React from 'react';
-import { Home } from 'routes';
+import {Home} from 'routes';
 
-export default () => <Home />;
+export default ({data}) => {
+  return <Home data={data} />;
+};
+
+export const query = graphql`
+  query InstaFeedQuery {
+    ...InstagramImages
+  }
+`;
