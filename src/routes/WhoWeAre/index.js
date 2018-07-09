bac4e3bfd2fc27f27f98bb4c3bbe664f35b512cb
Matthew Smith <azamatsmith@gmail.com>
2018-07-09 08:19:10 -0600

    /who-we-are copy updates

--- src/routes/WhoWeAre/index.js
+++ src/routes/WhoWeAre/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { P, UnderlineText } from 'components';
+import {P, UnderlineText} from 'components';
 import Profile from './components/Profile';
 
 export default class WhoWeAre extends React.Component {
@@ -16,14 +16,14 @@ export default class WhoWeAre extends React.Component {
   static defaultProps = {};
 
   render() {
-    const { mattProfile, rachelProfile } = this.props;
+    const {mattProfile, rachelProfile} = this.props;
     const pStyle = 'measure-wide lh-copy';
     return (
       <div className="WhoWeAre">
         <div className="flex flex-column items-center mh3">
           <UnderlineText
             extraStyles="mb0"
-            highlightStyle={{ left: '5px', width: '132px' }}
+            highlightStyle={{left: '5px', width: '132px'}}
             text="who we are"
           />
           <div className="tc">
@@ -38,7 +38,7 @@ export default class WhoWeAre extends React.Component {
             </P>
             <P extraStyles={pStyle}>
               Matt Smith is a full-stack developer. He has developed software
-              for the US Airforce, Department of Veteran Affairs, numerous
+              for the US Air Force, Department of Veterans Affairs, numerous
               startups, as well as launched several of his own apps and
               projects.
             </P>
