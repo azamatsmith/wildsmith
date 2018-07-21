3a53e62d60d3f27b771ed495499031bdd6ee6cde
Matthew Smith <azamatsmith@gmail.com>
2018-07-21 09:07:15 -0600

    Add storybook addon-options

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import {addDecorator, configure, setAddon} from '@storybook/react';
+import {setOptions} from '@storybook/addon-options';
 import JSXAddon from 'storybook-addon-jsx';
 import '../src/styles/main.scss';
 
-const req = require.context('../src/components', true, /\.stories\.js$/);
+setOptions({
+  name: 'Wildsmith',
+  sortStoriesByKind: true,
+  url: 'https://github.com/wildsmithstudio/wildsmith',
+});
 
 setAddon(JSXAddon);
 addDecorator(story => (
   <div className="sans-serif mv5 flex justify-center">{story()}</div>
 ));
 
+const req = require.context('../src/components', true, /\.stories\.js$/);
 function loadStories() {
   req.keys().forEach(filename => req(filename));
 }
