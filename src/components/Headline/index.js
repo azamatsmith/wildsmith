efd5458de32fb963e288c17e2b09f743f7d838fc
Matt Smith <azamatsmith@gmail.com>
2019-02-12 16:56:51 -0700

    Create <Headline />

---
+++ src/components/Headline/index.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+const Headline = styled.span`
+  // font-family: WorkSans-Medium;
+  font-size: 64px;
+  letter-spacing: 0;
+`;
+
+export default Headline;
--- src/components/HomepageHero/index.js
+++ src/components/HomepageHero/index.js
@@ -1,17 +1,24 @@
 import React from 'react';
 // import PropTypes from 'prop-types';
 import styled from 'styled-components';
+import {Container, Headline} from 'components';
 import Logo from './Logo';
 
-const StyledDiv = styled.div`
-  ${props => console.log('props yo', props) || ''}
+const StyledDiv = styled.div``;
+
+const StyledHeadline = styled(Headline)`
+  max-width: 902px;
 `;
 
 function HomepageHero() {
   return (
     <div className="HomepageHero vh-100 w-100">
       <Logo />
-      <StyledDiv />
+      <Container className="flex items-center">
+        <StyledHeadline className="black tc" as="h1">
+          We design and build digital experiences that matter
+        </StyledHeadline>
+      </Container>
     </div>
   );
 }
