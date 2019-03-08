113b836594cde0835aa2190fc85b8f4b4aedf8d3
Matt Smith <azamatsmith@gmail.com>
2019-03-08 06:07:57 -0700

    Upgrade storybook

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,17 +1,18 @@
 const path = require('path');
 // const defaultConfig = require('@storybook/react/dist/server/config/defaults/webpack.config.js');
 
-module.exports = (baseConfig, env, defaultConfig) => {
-  console.log(defaultConfig);
-  defaultConfig.module.rules.push({
+// module.exports = (baseConfig, env, defaultConfig) => {
+module.exports = ({config, mode}) => {
+  // console.log(defaultConfig);
+  config.module.rules.push({
     test: /\.scss$/,
     loaders: ['style-loader', 'css-loader', 'sass-loader'],
     include: path.resolve(__dirname, '../'),
   });
-  defaultConfig.module.rules.push({
+  config.module.rules.push({
     test: /\.svg$/,
     loaders: ['svg-inline-loader'],
     // include: path.resolve(__dirname, '../'),
   });
-  return defaultConfig;
+  return config;
 };
