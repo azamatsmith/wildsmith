71f71385cf568addbf69cef90aea3fc7dcbce0c8
Matt Smith <azamatsmith@gmail.com>
2019-03-08 06:08:33 -0700

    Remove comments

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,9 +1,6 @@
 const path = require('path');
-// const defaultConfig = require('@storybook/react/dist/server/config/defaults/webpack.config.js');
 
-// module.exports = (baseConfig, env, defaultConfig) => {
 module.exports = ({config, mode}) => {
-  // console.log(defaultConfig);
   config.module.rules.push({
     test: /\.scss$/,
     loaders: ['style-loader', 'css-loader', 'sass-loader'],
